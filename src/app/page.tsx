dfdac6accd36d9272a687edae5ec2488a7fff92f
piyushdev9000 <piyushswe009@gmail.com>
2024-11-14 21:40:05 +0530

    third

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,9 @@
-import Header from '@/components/Header/Header'
-import CategoryList from '@/components/CategoryList/CategoryList'
-import Slider from '@/components/Slider/Slider'
-import Products from '@/components/Products/Products'
-import Products2 from '@/components/Products/Products2'
-import Products3 from '@/components/Products/Products3'
+import Home from './home/page'
 
-export default function Home() {
+export default function Page() {
   return (
     <div>
-      <Header />
-      <CategoryList />
-      <Slider />
-      <Products Title={'Best of Electronics'} />
-      <Products Title={'Beauty, Food, Toys & More'} />
-      <Products2 Title={'Best Gadgets'} />
-      <div className="w-[98%] gap-2 mx-auto mt-2 flex h-[620px]">
-        <Products3 Title={'Winter Essentials'} />
-        <Products3 Title={'Home Decor'} />
-        <Products3 Title={'Gifts for All Occasions'} />
-      </div>
+      <Home />
     </div>
   )
 }
--- src/components/Products/Products2.tsx
+++ src/components/Products/Products2.tsx
@@ -1,17 +1,19 @@
-import CreateProduct from '@/components/CreateProduct';
-import { ProductsData2 } from '@/DummyData';
-import cover from "@public/cover.png";
-import Image from 'next/image';
+import CreateProduct from '@/components/CreateProduct'
+import { ProductsData2 } from '@/DummyData'
+import cover from '@public/cover.png'
+import Image from 'next/image'
 
 interface Category {
-  Title: string;
+  Title: string
 }
 
 export default function Products2({ Title }: Category) {
   return (
     <div className="w-[98%] gap-2 mx-auto mt-2 flex h-[625px]">
       <div className="flex-[1] w-2/5 bg-[var(--white-bg)]">
-        <p className="text-black font-semibold w-full h-fit p-4 text-xl">{Title}</p>
+        <p className="text-black font-semibold w-full h-fit p-4 text-xl">
+          {Title}
+        </p>
         <main className="grid grid-cols-2 gap-2 mx-4">
           {ProductsData2.map((product) => (
             <CreateProduct
@@ -23,9 +25,15 @@ export default function Products2({ Title }: Category) {
           ))}
         </main>
       </div>
-      <div className="flex-[2] h-full">
-        <Image src={cover} alt="cover" layout="responsive" width={1500} height={2000} className="object-cover h-full w-full" />
+      <div className="flex-[2] h-[625px] overflow-hidden">
+        <Image
+          src={cover}
+          alt="cover"
+          height={5000}
+          width={2000}
+          className="object-cover w-full h-full"
+        />
       </div>
     </div>
-  );
+  )
 }
